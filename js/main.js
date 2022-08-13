ad718fef7bafe25b7499d210632a5e221bacca85
Rafael Santos <contactrafaelsantos@gmail.com>
2022-08-13 17:32:30 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function changeColor() {
+    
+    let r = Math.floor(Math.random() * 255);
+        g = Math.floor(Math.random() * 255);
+        b = Math.floor(Math.random() * 255);
+
+    document.body.style.backgroundColor = 'rgb('+r+', '+g+', '+b+')'
+
+}
